0c9fc2eddf3d11435de5d790a600108767255f26
francomoglia496 <francomoglia496@gmail.com>
2018-07-10 11:36:11 -0300

    corrections - topic 1

--- Topic_1/src/main/java/Main.java
+++ Topic_1/src/main/java/Main.java
@@ -1,19 +1,11 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
-
 public class Main {
 
     public static void main(String[] args) {
-
-        Configurator.initialize(new DefaultConfiguration());
-        Logger log = LogManager.getLogger();
 
         CrearConexion cc = new CrearConexion("user1","pass","localhost/");
 
         cc.connectDB();
 
-
+        System.out.println("Conectado: " + cc.getConected());
     }
 }
